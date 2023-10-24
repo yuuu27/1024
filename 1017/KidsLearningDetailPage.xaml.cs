0eeccaeebf7db3385ef737a8409dee259af04f12
陳芊妤 <103488311+yuuu27@users.noreply.github.com>
2023-10-24 15:10:00 +0800

    Update KidsLearningDetailPage.xaml.cs

--- 1017/KidsLearningDetailPage.xaml.cs
+++ 1017/KidsLearningDetailPage.xaml.cs
@@ -2,26 +2,30 @@ namespace _1017;
 
 public partial class KidsLearningDetailPage : ContentPage
 {
-	public KidsLearningDetailPage(string categoryName)
-	{
-		InitializeComponent();
-		DisplayLearningResult(categoryName);
-		Title = categoryName;
+    public KidsLearningDetailPage(string categoryName)
+    {
+        InitializeComponent();
+        DisplayLearningResult(categoryName);
+        Title = categoryName;
     }
 
     private void DisplayLearningResult(string categoryName)
     {
-		LearningData learningData = new LearningData();
-		switch(categoryName) 
-		{
-			case "Numbers":
-				CvKids.ItemsSource = learningData.Numbers; break;
+        LearningData learningData = new LearningData();
+        switch (categoryName)
+        {
+            case "Numbers":
+                CvKids.ItemsSource = learningData.Numbers; break;
             case "Letters":
                 CvKids.ItemsSource = learningData.Letters; break;
             case "Shapes":
                 CvKids.ItemsSource = learningData.Shapes; break;
             case "Colors":
                 CvKids.ItemsSource = learningData.Colors; break;
+            case "Fruits":
+                CvKids.ItemsSource = learningData.Fruits; break;
+            case "Animals":
+                CvKids.ItemsSource = learningData.Animals; break;
             default:
                 break;
         }
